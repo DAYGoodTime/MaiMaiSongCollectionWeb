838a54b2d6db3194a6f2d200c83cd34267bc4ff5
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2025-08-22 21:54:19 +0800

    usagi api auth change

--- src/api/usagi.ts
+++ src/api/usagi.ts
@@ -6,6 +6,7 @@ export const fcMapping = ["app", "ap", "fcp", "fc"]
 export const fsMapping = ["sync", "fs", "fsp", "fsd", "fsdp"]
 
 const HOST = "https://uc.turou.fun/api/maimai/v1"
+const DEV_TOKEN = import.meta.env.VITE_USAGI_DEV_TOKEN
 
 export const queryUsagiUserScore = (uuid: String): Promise<Response<UsagiScore[]>> => {
     if (uuid.length === 0) {
@@ -14,6 +15,7 @@ export const queryUsagiUserScore = (uuid: String): Promise<Response<UsagiScore[]
     // 准备请求头
     const headers: Record<string, string> = {
         'Content-Type': 'application/json',
+        'x-developer-token': DEV_TOKEN
     };
     const url = `${HOST}/scores?uuid=${uuid}`
     const requestOption: RequestInit = {
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,6 +9,7 @@ interface ImportMetaEnv {
     readonly VITE_LXNS_OAUTH_APP_ID: string
     readonly VITE_LXNS_OAUTH_APP_SECRET: string
     readonly VITE_LXNS_OAUTH_APP_REDIRECT_URI: string
+    readonly VITE_USAGI_DEV_TOKEN: string
 }
 
 interface ImportMeta {
