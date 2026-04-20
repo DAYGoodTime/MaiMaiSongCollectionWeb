f68099e54b938dd899026e719c1be0843eb5b989
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2026-04-20 10:00:16 +0800

    misc.: remove useless

--- src/api/usagi.ts
+++ src/api/usagi.ts
@@ -1,29 +1,8 @@
-import type { UsagiResponse, UsagiScore } from "@/types/usagi"
+import type { UsagiScore } from "@/types/usagi"
 import apiRouterClient from "./router";
 export const rateMapping = ["sssp", "sss", "ssp", "ss", "sp", "s", "aaa", "aa", "a", "bbb", "bb", "b", "c", "d"]
 export const fcMapping = ["app", "ap", "fcp", "fc"]
 export const fsMapping = ["sync", "fs", "fsp", "fsd", "fsdp"]
-
-// const HOST = import.meta.env.VITE_USAGI_API_HOST
-// const DEV_TOKEN = import.meta.env.VITE_USAGI_DEV_TOKEN
-
-// import { createApiClient } from './base';
-// import apiRouterClient from "./router"
-
-// async function handleUsagiError(response: Response) {
-//     const errorBody = await response.json();
-//     return {
-//         message: `Usagi API Error: ${errorBody.detail || 'Unknown error'}`,
-//         body: errorBody
-//     };
-// }
-// const usagiApiClient = createApiClient({
-//     baseUrl: HOST,
-//     defaultHeaders: {
-//         'x-developer-token': DEV_TOKEN,
-//     },
-//     handleError: handleUsagiError,
-// });
 
 const UsagiService = {
     queryUsagiUserScore: (uuid: string): Promise<UsagiScore[]> => {
